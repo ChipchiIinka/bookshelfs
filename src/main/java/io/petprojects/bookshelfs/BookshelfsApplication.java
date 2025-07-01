6024c79fd3fd5c9766a3546c87acaf32821158d3
Mark Gizbrekht <m.gi2016@yandex.ru>
2025-07-01 15:38:46 +0300

    Initial Commit

---
+++ src/main/java/io/petprojects/bookshelfs/BookshelfsApplication.java
@@ -0,0 +1,13 @@
+package io.petprojects.bookshelfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshelfsApplication.class, args);
+    }
+
+}
